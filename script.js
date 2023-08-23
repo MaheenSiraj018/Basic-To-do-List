12a42e81e36f8241cc51fbe4fd38e3f8ddcb31e3
Maheen Siraj <113521903+MaheenSiraj018@users.noreply.github.com>
2023-08-23 12:27:11 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,59 @@
+var todolist = new Array();
+function additem() {
+    var task = document.getElementById('title').value;
+    todolist.push(task);
+    console.log(todolist);
+}
+function display() {
+    clear();
+    var task = document.getElementById('title').value;
+    const items = document.getElementById('items');
+    const creatediv = (task, index) => {
+        const div = document.createElement('div');
+        div.classList.add('todoitems');
+        div.style.display = 'flex';
+        div.style.flexDirection = 'row';
+        div.style.alignItems = 'center';
+        div.style.justifyContent = 'space-between';
+        div.style.width = '75vw';
+        div.style.alignSelf = 'center';
+        const li = document.createElement('h5');
+        li.style.fontSize = '20px';
+        li.style.fontWeight = '100';
+        li.textContent = task;
+        const button = document.createElement('button');
+        button.textContent = 'Remove Task';
+        button.addEventListener('click', () => removeitem(index));
+        const button1 = document.createElement('button');
+        button1.textContent = 'Edit Task';
+        button1.addEventListener('click', () => edititem(index));
+        div.appendChild(li);
+        div.appendChild(button1);
+        div.appendChild(button);
+        items.appendChild(div);
+        console.log(index, 'i am here');
+
+    }
+    todolist.forEach(func = (task, index) => {
+        creatediv(task, index);
+    });
+}
+function removeitem(index) {
+    todolist.splice(index, 1);
+    console.log(todolist);
+    display();
+}
+function adddisplay() {
+    additem();
+    display();
+}
+function clear() {
+    document.getElementById('items').innerHTML = "";
+}
+
+function edititem(index){
+    var editedtask = prompt("Edit task");
+    todolist[index]= editedtask;
+    display();
+
+}
